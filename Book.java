09eda7aea99f3b6eb7766c0798422dd8bad05ff2
Crich78 <31867941+Crich78@users.noreply.github.com>
2017-09-22 16:07:17 -0400

    Did the first few parts
    2.83, 2.84, 2.85
    Created accessors for String fields
    Created a print method for private fields
    Modified an object and added "pages" field

--- Book.java
+++ Book.java
@@ -3,24 +3,48 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author (Connor Richardson)
+ * @version (9/22/2017)
  */
 class Book
 {
     // The fields.
     private String author;
     private String title;
+    
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages =  bookPages;
     }
 
     // Add the methods here ...
+    
+   public String getAuthor()
+   {
+     return author;  
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public int getPages()
+    {
+        return pages;
+    }
+    
+    System.out.println (author);
+    
+    System.out.println (title);
+    
+    System.out.println (pages);
+    
 }
